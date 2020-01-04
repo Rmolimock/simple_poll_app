cb456b5f0f06ee5cfb08e28fd296b878bc4aa8be
Rmolimock <495@holbertonschool.com>
2020-01-04 06:11:02 +0000

    allow users to create questions with a dynamic number of answer selections

--- polls/urls.py
+++ polls/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -5,7 +7,10 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>', views.detail, name='detail'),
+    path('<int:question_id>', views.answers, name='answers'),
     path('<int:question_id>/results/', views.results, name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote')
-]
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('draft/', views.draft, name='draft'),
+    path('create_question/', views.create_question, name='create_question'),
+    path('add_answer/', views.add_answer, name='add_answer'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- polls/views.py
+++ polls/views.py
@@ -10,12 +10,12 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
+def answers(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
+    return render(request, 'polls/answers.html', {'question': question})
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -26,10 +26,34 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html', {
+        return render(request, 'polls/answers.html', {
             'question': question,
             'error_message': 'Please select a choice.'})
     else:
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def draft(request):
+    return render(request, 'polls/draft.html')
+
+def create_question(request):
+    from django.utils import timezone
+    q_text = request.POST['question_text']
+    q = Question(pub_date=timezone.now(), question_text=q_text)
+    q.save()
+    if len(request.POST['choice_text_a']) > 0:
+        choice = Choice(question=q, votes=0, choice_text=request.POST['choice_text_a'])
+        choice.save()
+    if len(request.POST['choice_text_b']) > 0:
+        choice = Choice(question=q, votes=0, choice_text=request.POST['choice_text_b'])
+        choice.save()
+    for c_num in range(len(request.POST) - 4):
+        if len(request.POST['choice_text' + str(c_num)]) > 0:
+            choice = Choice(question=q, votes=0, choice_text=request.POST['choice_text' + str(c_num)])
+            choice.save()
+
+    return index(request)
+
+def add_answer(request):
+        return HttpResponseRedirect(reverse('polls:index'))
