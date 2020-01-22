bf6492ad7ae9b488c3989e39a0cd805f1e437d4d
Rmolimock <495@holbertonschool.com>
2020-01-22 02:03:42 +0000

    user must enter question before creating 3+ answers

--- static/polls/add_question.js
+++ static/polls/add_question.js
@@ -1,14 +1,49 @@
 jQuery(function($) {
 var i = 0;
-$("#create_choice").click(function() {
-    $("ul > li:last-child").after('<li class="list-group-item"><label>Choice:</label> <input class="choice" type="text" name="choice_text' + String(i) + '"></input> <div class="btn btn-danger btn-sm remove_choice" id="remove_choice">X</div></li>');
-$('#submit')[0].scrollIntoView();
-i += 1;
-return false;
+$("#create_answer").click(function() {
+  if ($('#question_input').val() != '') {
+    $('#create_question_requirement_2').css('display', 'none');
+    if ($('#answer_a').val() != '') {
+      if ($('#answer_b').val() != '') {
+        if (i < 3) {
+          $('#create_question_requirement_1').css('display', 'none');
+          $("ul > li:last-child").after('<li class="list-group-item"><label>answer:</label> <input class="answer" type="text" name="answer_' + String(i) + '"></input><i id="remove_answer" class="float-right fas fa-times-circle"></i></li>');
+          $('#submit')[0].scrollIntoView();
+          i += 1;
+          return false;
+        }
+        else {
+          $('#create_question_requirement_0').css('display', 'inline');
+        }
+      }
+      else {
+        $('#create_question_requirement_1').css('display', 'inline');
+      }
+    }
+    else {
+      $('#create_question_requirement_1').css('display', 'inline');
+    }
+  }
+  else {
+    $('#create_question_requirement_2').css('display', 'inline');
+  }
 });
 
-$(document).on('click', '#remove_choice', function() {
+$('#question_input').change(function() {
+  if ($('#question_input').val() == '') {
+    alert('here');
+    $('#submit').prop('disabled', true);
+  }
+  else {
+    $('#submit').prop('disabled', false);
+  }
+});
+
+
+
+$(document).on('click', '#remove_answer', function() {
   $(this).parent().remove();
+  i -= 1;
 });
 
 
